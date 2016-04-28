ef5f00aa4656c4fee390e9ccc026998af672e793
David Brriga <dbarriga@itexico.net>
2016-04-28 11:54:02 -0500

    Creating repo

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var fs = require('fs');
+var _ = require('lodash');
+var app = express();
+var users = [];
+
+fs.readFile('users.json', {encoding: 'utf8'}, function(err, data) {
+    if (err) throw err;
+
+    JSON.parse(data).forEach(function(user) {
+        user.name.full = _.startCase(user.name.first + ' ' + user.name.last);
+        users.push(user);
+    })
+})
+app.get('/', function (req, res) {
+    var buffer = '';
+    users.forEach(function (user) {
+        buffer += '<a href="/'+user.username +'">' + user.name.full + '<br>';
+    })
+    res.send(buffer);
+});
+
+app.get(/.*dog.*/, function(req, res, next) {
+    console.log('DOGS GO WOOF');
+    next();
+});
+
+app.get(/big.*/, function (req, res, next) {
+    console.log('BIG USER ACCESS');
+    next();
+});
+
+app.get('/:username', function(req, res) {
+    var username = req.params.username;
+    res.send(username);
+});
+
+app.get('/yo', function (req, res) {
+  res.send('YO!');
+});
+var server = app.listen(3000, function () {
+  console.log('Server running at http://localhost:' + server.address().port);
+});

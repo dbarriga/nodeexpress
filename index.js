e3daeed519a40817fa8e296da2b77003b98673c6
David Brriga <dbarriga@itexico.net>
2016-04-28 12:15:06 -0500

    video 03

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var _ = require('lodash');
 var app = express();
 var users = [];
+var engines = require('consolidate');
 
 fs.readFile('users.json', {encoding: 'utf8'}, function(err, data) {
     if (err) throw err;
@@ -12,12 +13,13 @@ fs.readFile('users.json', {encoding: 'utf8'}, function(err, data) {
         users.push(user);
     })
 })
+app.engine('hbs', engines.handlebars);
+
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
 app.get('/', function (req, res) {
-    var buffer = '';
-    users.forEach(function (user) {
-        buffer += '<a href="/'+user.username +'">' + user.name.full + '<br>';
-    })
-    res.send(buffer);
+    res.render('index', {users: users});
 });
 
 app.get(/.*dog.*/, function(req, res, next) {
